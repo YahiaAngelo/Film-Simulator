74364efd464f68f411a75476690a23ad264841ac
Yahia <yahia.mostafa.elsayed@gmail.com>
2024-12-14 17:05:19 +0200

    Version 0.2

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.yahiaangelo.filmsimulator.android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.1.1"
+        versionCode = 4
+        versionName = "0.2"
     }
     buildFeatures {
         compose = true
